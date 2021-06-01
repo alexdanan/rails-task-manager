256af5c732284d05daf830f0aa8bc1714b282881
Alexia Danan <dananalexia@gmail.com>
2021-06-01 12:24:37 +0200

    Add task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_task
+
+  end
+end
